a4c14963ed3ee30ac14001393c930da9614e184d
Ed Bryce <edward@edwardbryce.com>
2023-03-27 20:37:07 +0100

    Added create account script
    Added script to create new account. Also made some tweaks to sign up page

--- includes/loginVerify.php
+++ includes/loginVerify.php
@@ -8,8 +8,7 @@
         if (!mysqli_stmt_prepare($stmt, $sql)){
             echo "<p> statement error </p>";
             exit();
-        }
-        else{
+        }else{
             mysqli_stmt_bind_param($stmt, "s", $email);
             mysqli_stmt_execute($stmt);
             $result = mysqli_stmt_get_result($stmt);
--- signup.php
+++ signup.php
@@ -23,24 +23,36 @@
 </nav>
 
 <!-- Sign Up Form -->
-<div class = "container d-flex align-items-center border" style = "max-height:600px; min-height:600px">
-    <div class = "container d-flex justify-content-center border">  
+<div class = "container d-flex align-items-center" style = "max-height:800px; min-height:800px">
+    <div class = "container d-flex justify-content-center">  
         <div class = "border-5 border-top border-primary shadow-lg" style = "width:50%;">
             <div class = "p-5 border border-2 border-top-0 shadow-lg">
-                <div class = "d-flex justify-content-center border">
+                <div class = "d-flex justify-content-center">
                     <h3>Create Account</h3> 
                 </div>
-                <form action="createAccount.php">
+                <form action="includes/createAccount.php" class="was-validated" method="post">
+                    <div class="mb-3 mt-3">
+                        <label for="username" class="form-label">Username:</label>
+                        <input type="username" class="form-control" id="username" placeholder="Enter username" name="username" required>
+                        <div class="invalid-feedback">Please enter a username</div>
+                    </div>
                     <div class="mb-3 mt-3">
                         <label for="email" class="form-label">Email:</label>
-                        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+                        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" required>
+                        <div class="invalid-feedback">Please enter an email.</div>
                     </div>
                     <div class="mb-3">
-                        <label for="pwd" class="form-label">Password:</label>
-                        <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+                        <label for="password" class="form-label">Password:</label>
+                        <input type="new-password" class="form-control" id="password" placeholder="Enter password" name="password" required>
+                        <div class="invalid-feedback">Please enter a password.</div>
                     </div>
-                    <div class="d-grid">
-                        <button class="btn btn-outline-dark" type="submit">Login</button>
+                    <div class="mb-3">
+                        <label for="passwordConfirm" class="form-label">Confirm Password:</label>
+                        <input type="new-password" class="form-control" id="passwordConfirm" placeholder="Re-enter password" name="passwordConfirm" required>
+                        <div class="invalid-feedback">Please re-enter your password.</div>
+                    </div>
+                    <div class="d-grid pt-2">
+                        <button class="btn btn-outline-dark" type="submit">Sign Up</button>
                     </div>
                     <div class = "pt-3 d-flex justify-content-center">
                         <p class="mb-0  text-center">Have an account? <a href="login.php" class="text-primary fw-bold">Log In</a></p>
