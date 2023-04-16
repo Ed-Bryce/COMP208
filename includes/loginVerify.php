754cc0ea5468166cde8efdd91975b8a98a9f63d3
NotTheRealTomJ <thomasjones952@gmail.com>
2023-04-16 17:07:43 +0100

    Refactor code

--- includes/loginVerify.php
+++ includes/loginVerify.php
@@ -5,33 +5,31 @@
     $password = $_POST["password"];
     $sql = "SELECT * FROM chessdb.Users WHERE email=?;";
     $stmt = mysqli_stmt_init($conn);
-        if (!mysqli_stmt_prepare($stmt, $sql)){
-            exit();
-        }else{
-            mysqli_stmt_bind_param($stmt, "s", $email);
-            mysqli_stmt_execute($stmt);
-            $result = mysqli_stmt_get_result($stmt);
-            if($row = mysqli_fetch_assoc($result)){
-                if (password_verify($password, $row["password"])) {
-                    echo 'Password is valid!';
-                    $_SESSION["logged_in"] = "true";
-                    $_SESSION["failed"] = "false";
-                    $_SESSION["userID"] = $row["userID"];
-                    echo "<script>alert('data saved succesfully');</script>";
-                    header("Location: ../board.php");
-                    exit;
-                }
-                else{
-                    $_SESSION["failed"] = "true";
-                    header("Location: ../login.php");
-                    
-                }
+    if (!mysqli_stmt_prepare($stmt, $sql)){
+        exit();
+    }else{
+        mysqli_stmt_bind_param($stmt, "s", $email);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        if($row = mysqli_fetch_assoc($result)){
+            if (password_verify($password, $row["password"])) {
+                echo 'Password is valid!';
+                $_SESSION["logged_in"] = "true";
+                $_SESSION["failed"] = "false";
+                $_SESSION["userID"] = $row["userID"];
+                echo "<script>alert('data saved succesfully');</script>";
+                header("Location: ../board.php");
+                exit;
             }
-            else {
+            else{
                 $_SESSION["failed"] = "true";
                 header("Location: ../login.php");
-                
             }
-            exit;
         }
+        else {
+            $_SESSION["failed"] = "true";
+            header("Location: ../login.php");
+        }
+        exit;
+    }
 ?>
