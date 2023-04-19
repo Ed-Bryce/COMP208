c120e0f5bcbcbb0e219a217120491213085e4774
NotTheRealTomJ <thomasjones952@gmail.com>
2023-04-19 17:01:16 +0100

    Add/Change change password menu link

--- social.php
+++ social.php
@@ -230,6 +230,7 @@
     </div>
     <div class="offcanvas-body">
     <!-- sidebar body -->
+        <a type="button" class="btn" href="changePassword.php">Change Password</a>
     </div>
     </div>
 
