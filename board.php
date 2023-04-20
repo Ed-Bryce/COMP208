495b3c7dc08837120cd625f492d23739f05fb611
NotTheRealTomJ <thomasjones952@gmail.com>
2023-04-20 17:20:27 +0100

    Fixed looking for stylesheet

--- board.php
+++ board.php
@@ -14,7 +14,7 @@ $username = $_SESSION["username"];
     <title>Chess Game - Play</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="stylesheet.css">
 </head>
 
 <body style="background-color: white;">
