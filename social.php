327302f5f418493b21cb62740f4155620b364e94
NotTheRealTomJ <thomasjones952@gmail.com>
2023-04-20 16:55:13 +0100

    Adj layout, fixed chat and added remove friends

--- social.php
+++ social.php
@@ -6,7 +6,7 @@ $username = $_SESSION["username"];
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" style="height:100vh">
 
 <head>
     <link rel="stylesheet" href="styling/styling.css">
@@ -21,7 +21,7 @@ $username = $_SESSION["username"];
     <link rel="stylesheet" href="style.css">
 </head>
 
-<body>
+<body style="height:100vh">
     <!-- NAVIGATION BAR ######################################### -->
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
         <div class="container justify-content-start">
@@ -45,10 +45,10 @@ $username = $_SESSION["username"];
 
     <!-- body -->
     <div class="container mt-5">
-        <div class="row">
-            <div class="col-lg-4 mb-4">
-                <div class="card">
-                    <h3>Friend Request Out</h3>
+        <div class="row" style="height:100vh">
+            <div class="col-md-4" style="height:100vh">
+                <div class="card p-2" style="height:80vh">
+                    <h3 class="text-center">Send Friend Request</h3>
                     <form method="post" action="social.php">
                         <div class="input-group mb-3">
                             <input type="text" name="reqout" class="form-control" placeholder="New friend">
@@ -82,7 +82,7 @@ $username = $_SESSION["username"];
                     }
                     ?>
                     <hr>
-                    <h3>Friend Requests In</h3>
+                    <h3 class="text-center">Recieved Friend Requests</h3>
                     <table>
                         <?php
                         //GET INCOMING REQUESTS FROM DB
@@ -92,15 +92,21 @@ $username = $_SESSION["username"];
                         WHERE chessdb.FriendRequests.to_user = ? AND chessdb.Users.userID = chessdb.FriendRequests.from_user;";
                         mysqli_stmt_prepare($stmt, $sql);
                         mysqli_stmt_bind_param($stmt, "i", $id);
-                        mysqli_execute($stmt);
+                        mysqli_stmt_execute($stmt);
                         $results = mysqli_stmt_get_result($stmt);
+
 
                         while ($row = mysqli_fetch_assoc($results)) {
                             $friendReqId = $row["userID"];
                             $yes = True;
                             $no = False;
-                            echo "<p>" . $row["username"] . " <a href='social.php?accept=$yes&friendReqId=$friendReqId'>YES</a> <a href='social.php?accept=$no&friendReqId=$friendReqId'>NO</a> <p>";
+                            echo "<p>"
+                                . $row['username'] . " 
+                            <a class='float-end mx-2 text-decoration-none font-weight-bold' href='social.php?accept=$no&friendReqId=$friendReqId'>DENY</a>
+                            <a class='float-end mx-2 text-decoration-none font-weight-bold' href='social.php?accept=$yes&friendReqId=$friendReqId'>ACCEPT</a>
+                            </p>";
                         }
+
 
                         //RESPONSE
 
@@ -129,31 +135,39 @@ $username = $_SESSION["username"];
                 </div>
             </div>
 
-            <div class="col-lg-4 mb-4">
-                <div class="card">
-                    <h3>Friends List</h3>
+            <div class="col-md-4">
+                <div class="card p-2" style="height:80vh">
+                    <h3 class="text-center">Friends List</h3>
                     <table>
                         <!-- GET FRIENDS LIST FROM DB -->
                         <?php
+
                         $stmt = mysqli_stmt_init($conn);
                         $sql = "SELECT userID, username
-                    FROM chessdb.Friends, chessdb.Users
-                    WHERE (chessdb.Friends.userID_1 = ? OR chessdb.Friends.userID_2 = ?) 
-                    AND (chessdb.Users.userID = chessdb.Friends.userID_1 OR chessdb.Users.userID = chessdb.Friends.userID_2);";
+                        FROM chessdb.Friends, chessdb.Users
+                        WHERE (chessdb.Friends.userID_1 = ? OR chessdb.Friends.userID_2 = ?) 
+                        AND (chessdb.Users.userID = chessdb.Friends.userID_1 OR chessdb.Users.userID = chessdb.Friends.userID_2);";
                         mysqli_stmt_prepare($stmt, $sql);
                         mysqli_stmt_bind_param($stmt, "ii", $id, $id);
                         mysqli_stmt_execute($stmt);
                         $result = mysqli_stmt_get_result($stmt);
 
                         //LOOP THROUGH REQUESTS AND DISPLAY
+                        //echo "<div class='list-group'>";
                         while ($row = mysqli_fetch_assoc($result)) {
                             if ($row["userID"] != $id) {
                                 $friendId = $row["userID"];
                                 $friendUsername = $row["username"];
                                 $_SESSION["friendID"] = $friendId;
-                                echo "<p><a href='social.php?friend=$friendUsername&id=$friendId'>" . $friendUsername . "</a></p>";
+                                echo "<p>
+                                <a>$friendUsername</a>
+                                <a class='float-end mx-1 text-decoration-none font-weight-bold text-danger' href='includes/removeFriend.php?id=$friendId'>Remove</a>
+                                <a class='float-end me-5 text-decoration-none text-primary' href='social.php?friend=$friendUsername&id=$friendId'>Message</a>
+                                </p>";
                             }
                         }
+
+                        
 
 
                         ?>
@@ -161,37 +175,40 @@ $username = $_SESSION["username"];
                 </div>
                 </table>
             </div>
-        </div>
-
-        <div class="col-lg-4 mb-4">
-            <div class="card">
-                <h3>Chat with friends</h3>
-                <?php
-                // causes error but breaks everything if removed
-                $friend = $_GET["friend"];
-                $friendID = $_GET["id"]
-                ?>
-                <form method="post" action="includes/sendMessage.php">
-                    <input type="hidden" name="id" value="<?= $id ?>">
-                    <div class="input-group mb-3">
-                        <input type="text" name="chatfriend" class="form-control" placeholder="Friend" value=<?= $friendUsername ?> readonly>
-                        <button type="button" class="btn btn-primary" onClick="location.href='social.php?id=<?= $id ?>&p=<?= $get ?>'">To Game</button>
-                    </div>
-                    <div class="form-group">
-                        <textarea class="form-control" id="textbox" placeholder="Enter email" name="textbox" rows="5"></textarea>
-                    </div>
-                    <button type="submit" class="btn btn-primary mt-2">SEND</button>
-                </form>
-                <?php
-                // Working now 
-                //SEND MESSAGE (moved to own file -> sendMessage.php)
-                //DISPLAY MESSSAGES (moved to own file -> getMessages.php)
-                include "includes/getMessages.php";
-                ?>
-                <iframe width="100%" height="500" src="chat.php?id=$id&id=<?= $id ?>" title="chats" frameBorder="1"></iframe>
+            <div class="col-md-4">
+                <div class="card p-2" style="height:80vh">
+                    <h3 class="text-center">Chat with friends</h3>
+                    <?php
+                    // causes error but breaks everything if removed
+                    $friend = $_GET["friend"];
+                    $friendID = $_GET["id"]
+                    ?>
+                    <form method="post" action="includes/sendMessage.php">
+                        <input type="hidden" name="id" value="<?= $friendID ?>">
+                        <input type="hidden" name="username" value="<?= $friend ?>">
+                        <div class="input-group mb-3">
+                            <input type="text" name="chatfriend" class="form-control" placeholder="Friend" value=<?= $friend ?> readonly>
+                            <button type="button" class="btn btn-primary" onClick="location.href='board.php'">To Game</button>
+                        </div>
+                        <div class="form-group">
+                            <textarea class="form-control" id="textbox" placeholder="Enter message" name="textbox" rows="5"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary mt-2">SEND</button>
+                    </form>
+                    <?php
+                    // Working now 
+                    //SEND MESSAGE (moved to own file -> sendMessage.php)
+                    //DISPLAY MESSSAGES (moved to own file -> getMessages.php)
+                    include "includes/getMessages.php";
+                    ?>
+                </div>
             </div>
         </div>
+
+
     </div>
+
+
     </div>
 
     <!-- Sidebar -->
